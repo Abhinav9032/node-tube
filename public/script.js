3d8fe2424642abd858fcbb96e73ac777ef67f05d
Abhinav Mishra <abhinav.mishra@cerner.com>
2020-08-22 20:33:15 +0530

    I can see my video on browser

---
+++ public/script.js
@@ -0,0 +1,38 @@
+console.log('From script.js');
+
+const socket  = io('/')
+const videoGrid = document.getElementById('video-grid');
+const myVideo = document.createElement('video');
+
+myVideo.muted = true;
+
+
+
+let myVideoStream;
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio:true,
+}).then( stream => {
+    myVideoStream = stream;
+    addVideoStream(myVideo , stream);
+})
+
+socket.emit('join-room' , ROOM_ID);
+
+socket.on('user-connected' , () => {
+    connecToNewUser();
+})
+
+const connecToNewUser = () => {
+ console.log('user-connected');
+}
+
+
+const addVideoStream = (video , stream) => {
+    video.srcObject= stream;
+    video.addEventListener('loadedmetadata' , () => {
+        video.play();
+    })
+    videoGrid.append(myVideo);
+
+}
